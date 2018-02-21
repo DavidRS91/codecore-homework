4ee19b33d78cd9c8a2fbc6030ca7695ef8902f6a
David Scott <david.r.scott91@gmail.com>
2018-02-20 17:45:08 -0800

    add user model and controller

--- blog_on_rails/config/routes.rb
+++ blog_on_rails/config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments, only: [:create,:destroy], shallow: true
   end
+  resources :users, only: [:new, :create, :edit, :up]
+  resource :session, only: [:new, :create, :destroy]
 end
